f2d731af18e9b60cb944e925a9fe9fef17967437
Nischitha S J <48064956+nishujayaraj@users.noreply.github.com>
2026-07-08 17:00:27 -0700

    Add: longest-substring-without-duplicates - submission-0

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        l = 0
+        max_len = 0
+        hashmap = {}
+        
+        for r in range(len(s)):
+            # if s[r] already in window, shrink from left until it's gone
+            while s[r] in hashmap:
+                del hashmap[s[l]]
+                l += 1
+            
+            # add current character to window
+            hashmap[s[r]] = True
+            
+            # update max window size
+            max_len = max(max_len, r - l + 1)
+        
+        return max_len
